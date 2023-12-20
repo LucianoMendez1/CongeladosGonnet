3baad9a8d5dea4d0045e8250d3bbf61ee789ab88
luciano mendez <mendezluciano23@gmail.com>
2023-12-19 23:08:11 -0300

    push

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -4,25 +4,25 @@ const Description = () => {
 
     const arrBook = [
         {
+          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1703034663/1_vtxvj0.jpg'
+        },
+        {
+          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1703034660/14_himxum.jpg'
+        },
+        {
+          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1703034655/7_gpykl4.jpg'
+        },
+        {
+          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1703034657/10_tycpba.jpg'
+        },
+        {
+          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1703034378/Congelados%20gonnet/4_ashxbl.jpg'
+        },
+        {
+          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1703034654/6_fxvxsx.jpg'
+        },
+        {
           img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836522/Congelados%20gonnet/312708304_633817484894696_6648427633348045866_n_h96qc7.jpg'
-        },
-        {
-          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836385/Congelados%20gonnet/166610826_807897109828557_6990238484478294060_n_gg4wae.jpg'
-        },
-        {
-          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836385/Congelados%20gonnet/292106793_441086904270357_8898818312377327728_n_adcqez.jpg'
-        },
-        {
-          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836523/Congelados%20gonnet/334503955_139769822341679_2424654153657741900_n_z1suoq.jpg'
-        },
-        {
-          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836385/Congelados%20gonnet/280656954_515481630050281_911059872857348767_n_xttxvi.jpg'
-        },
-        {
-          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836385/Congelados%20gonnet/339978298_1054561692170116_2856319343052655225_n_cw7u4t.jpg'
-        },
-        {
-          img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836385/Congelados%20gonnet/292106793_441086904270357_8898818312377327728_n_adcqez.jpg'
         },
         {
           img:'https://res.cloudinary.com/dvnhn35l4/image/upload/v1699836385/Congelados%20gonnet/386305546_821620323026161_8580771787723342863_n_vtz9zo.jpg'
--- src/components/presentacion/Presentacion.jsx
+++ src/components/presentacion/Presentacion.jsx
@@ -1,21 +1,22 @@
+import "./presentacion.css";
+import Description from "../description/Description";
 
-import './presentacion.css';
-import Description from '../description/Description';
 const Presentacion = () => {
-    
-
-    return (
-        <div>
-            <div className="container-presentation">
-                <div className="title"><h1>Congelados Gonnet</h1></div>
-                <div className="subtitle">Hace tu Pedido</div>
-            
-            </div>
-            
-            <Description/>
+  return (
+    <div>
+      <div className="container-presentation">
+        <div className="title">
+          <h1>Congelados Gonnet</h1>
+       
+        <div className="subtitle">
+          Hace tu Pedido online.
+          <button className="button-presentacion">Ver Productos</button>
         </div>
-        
-    );
+      </div>
+      </div>
+      <Description />
+    </div>
+  );
 };
 
 export default Presentacion;
